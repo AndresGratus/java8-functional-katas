af6dcb862fcdfef759c9fc5a8f4a6da730c6d9e8
Andres <andres.castaneda280040@gmail.com>
2022-05-25 15:38:18 -0500

    Kata 1 Realizamos un array de tipo map que Guardara la lista, luego mostramos la lista con el map

--- src/main/java/katas/Kata1.java
+++ src/main/java/katas/Kata1.java
@@ -5,8 +5,11 @@ import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: use map() to project an array of videos into an array of {id, title}-pairs
@@ -16,7 +19,22 @@ import java.util.Map;
 public class Kata1 {
     public static List<Map> execute() {
         List<Movie> movies = DataUtil.getMovies();
+        //List<Movie> lista = new Arrays.asList("","","","");
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
+        List<Map> lista = new ArrayList<>();
+
+        //Movie o =(x) -> x.g
+
+        lista = movies.stream()
+                .map((x) ->ImmutableMap.of(x.getId(), x.getInterestingMoments(), x.getTitle(), x.getBoxarts())).collect(Collectors.toList());
+        return lista;
+
+
+//        Map<String, lista> mapa = movies.stream().collect(Collectors.toMap(x -> x.getId(), x -> x));
+//
+//        return ImmutableList.of(ImmutableMap.of(movie.getId, 5, "title", "Bad Boys"));
+//
+//        List < ModeloCoche > modelos = Arrays.asList(modelo1, modelo2, modelo3);
+
     }
 }
